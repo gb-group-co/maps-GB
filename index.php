611bc3c7dde11d713ee7efe210f2c528e3ce94ad
Julien Richard <44161762+JulienYvesRICHARD@users.noreply.github.com>
2019-06-03 14:02:39 +0200

    from index.html to index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
-    <?php
+  <?php
     header("Access-Control-Allow-Origin: *");
-    ?>
+  ?>
   <head>
+  
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
       <link rel="icon" href="../favicon.ico">
